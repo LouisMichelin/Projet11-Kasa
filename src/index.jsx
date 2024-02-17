addacfef93528609e6bbead60083197f6ccff850
LouisMichelin <louis.michelin097@gmail.com>
2024-02-17 20:47:59 +0100

    Creation of React App & Rooter + Init

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+ReactDOM.render(
+   <React.StrictMode>
+      <Router>
+         {/* <GlobalStyle />
+         <Header /> */}
+         <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="*" element={<Error />} /> */}
+         </Routes>
+      </Router>
+   </React.StrictMode>,
+   document.getElementById("root")
+);
