2cecff9bf1a4559fe4b083d072cce3673f45253b
LouisMichelin <louis.michelin097@gmail.com>
2024-02-19 18:02:50 +0100

    Finalisation style de Header + Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,19 @@ function Header() {
       <div className="HeaderWrapper">
          <img className="HeaderLogo" src={LogoBig} alt="Logo Kasa" />
          <nav className="NavContainer">
-            <Link className="NavLink" to="/">
+            <Link
+               className="NavLink"
+               to="/"
+               // style={{ textDecoration: "solid" }}
+            >
                Accueil
             </Link>
 
-            <Link className="NavLink" to="/1337">
+            <Link
+               className="NavLink"
+               to="/1337"
+               // style={{ textDecoration: "solid" }}
+            >
                A Propos
             </Link>
          </nav>
