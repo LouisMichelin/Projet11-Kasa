107f4d658f938a3bf0e6c2b8c67fe39d20b5b6eb
LouisMichelin <louis.michelin097@gmail.com>
2024-02-22 22:07:27 +0100

    Sauvegarde

--- src/components/MenusApropos/MenusApropos.jsx
+++ src/components/MenusApropos/MenusApropos.jsx
@@ -1,47 +1,46 @@
 import "./MenusApropos.scss";
 import ArrowBack from "../../assets/arrowBack.png";
 import { useState } from "react";
+import { aboutList } from "../../data/apropos";
 
-function Button({ name }) {
-   return (
-      <button className="MenuButton">
-         <div className="MenuTagName">{name}</div>
-         <img
-            className="MenuTagArrow"
-            alt="Flèche vers le haut"
-            src={ArrowBack}
-         />
-      </button>
-   );
-}
-
-function Menus() {
+function ButtonApropos() {
    const [isShown, setIsShown] = useState(false);
 
    const handleClick = () => {
-      isShown ? setIsShown(false) : setIsShown(true);
+      // isShown ? setIsShown(false) : setIsShown(true);
+      setIsShown(!isShown);
    };
 
    return (
       <div className="MenuWrapper">
-         <Button name="Fiabilité" />
-         <Button name="Respect" />
-         <Button name="Service" />
-         <Button name="Sécurité" />
-
-         {/* <button className="MenuButton" onClick={handleClick} id="1">
-            <div className="MenuTagName">Fiabilité</div>
-            <img
-               className="MenuTagArrow"
-               alt="Flèche vers le haut"
-               src={ArrowBack}
-            />
-         </button>
-         {isShown ? (
-            <div className="MenuHiddenDiv">HIDDEN MESSAGE 1</div>
-         ) : null} */}
+         {aboutList.map(({ id, title, content }) => (
+            <div>
+               <button key={id} className="MenuButton" onClick={handleClick}>
+                  <div className="MenuTagName">{title}</div>
+                  <img
+                     className="MenuTagArrow"
+                     alt="Flèche vers le haut"
+                     src={ArrowBack}
+                  />
+               </button>
+               {isShown ? <div>{content}</div> : null}
+            </div>
+         ))}
       </div>
+      // <div className="MenuWrapper">
+      //    <div>
+      //       <button className="MenuButton" onClick={handleClick}>
+      //          <div className="MenuTagName">title</div>
+      //          <img
+      //             className="MenuTagArrow"
+      //             alt="Flèche vers le haut"
+      //             src={ArrowBack}
+      //          />
+      //       </button>
+      //       {isShown ? <div>content</div> : null}
+      //    </div>
+      // </div>
    );
 }
 
-export default Menus;
+export default ButtonApropos;
--- src/pages/Propos/Propos.jsx
+++ src/pages/Propos/Propos.jsx
@@ -1,12 +1,12 @@
 import "./Propos.scss";
 import BackgroundImage from "../../components/Background/Background";
-import MenusApropos from "../../components/MenusApropos/MenusApropos";
+import ButtonApropos from "../../components/MenusApropos/MenusApropos";
 
 function Propos() {
    return (
       <div className="ProposWrapper">
          <BackgroundImage />
-         <MenusApropos />
+         <ButtonApropos />
       </div>
    );
 }
