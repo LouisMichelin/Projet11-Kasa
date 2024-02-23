513f8b0a30dc12c36161532d675034b1d799a180
LouisMichelin <louis.michelin097@gmail.com>
2024-02-23 17:37:02 +0100

    Home : Setup of Title & Background-Title-Image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.scss";
+import BackgroundImage from "../../components/Background/Background";
 
 function Home() {
    return (
-      <div className="HomeWrapper">
-         <h2>PAGE D'ACCUEIL</h2>
-      </div>
+      <figure className="HomeWrapper">
+         <h2 className="HomeTitle">Chez-vous, partout et ailleurs</h2>
+         <BackgroundImage />
+      </figure>
    );
 }
 
