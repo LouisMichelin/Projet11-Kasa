433e1560c8bd31a108d5381112cc17666a7188dc
LouisMichelin <louis.michelin097@gmail.com>
2024-02-27 22:18:54 +0100

    Save

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,16 +1,21 @@
 import "./Collapse.scss";
 import ArrowBack from "../../assets/arrowBack.png";
 
-function Collapse({ id, title, showHidden }) {
+function Collapse({ id, title, content, showHidden, isShown }) {
    return (
-      <button key={id} className="MenuButton" onClick={showHidden}>
-         <div className="MenuTagName">{title}</div>
-         <img
-            className="MenuTagArrow"
-            alt="Flèche vers le haut"
-            src={ArrowBack}
-         />
-      </button>
+      <div key={id} className="MenuWrapper">
+         <button key={id} className="MenuButton" onClick={showHidden}>
+            <div className="MenuTagName">{title}</div>
+            <img
+               className="MenuTagArrow"
+               alt="Flèche vers le haut"
+               src={ArrowBack}
+            />
+         </button>
+         <div className="AproposHiddenContent">
+            {isShown ? { content } : "NOPE"}
+         </div>
+      </div>
    );
 }
 
--- src/pages/Propos/Propos.jsx
+++ src/pages/Propos/Propos.jsx
@@ -6,36 +6,8 @@ import { aboutList } from "../../data/apropos";
 import { useState } from "react";
 
 function Propos() {
-   const [isShown0, setIsShown0] = useState(false);
-   const [isShown1, setIsShown1] = useState(false);
-   const [isShown2, setIsShown2] = useState(false);
-   const [isShown3, setIsShown3] = useState(false);
-
-   function handleClick(param) {
-      switch (param) {
-         case 0:
-            setIsShown0(!isShown0);
-            return alert(
-               `"param" vaut ${param} ; ici, on a comme value de isShown0 : ${isShown0}`
-            );
-         case 1:
-            setIsShown1(!isShown1);
-            return alert(
-               `"param" vaut ${param} ; ici, on a comme value de isShown0 : ${isShown1} ${`isShown${param}`}`
-            );
-         case 2:
-            setIsShown2(!isShown2);
-            return alert(
-               `"param" vaut ${param} ; ici, on a comme value de isShown0 : ${isShown2}`
-            );
-         case 3:
-            setIsShown3(!isShown3);
-            return alert(
-               `"param" vaut ${param} ; ici, on a comme value de isShown0 : ${isShown3}`
-            );
-         default:
-            return alert("rien ne passe");
-      }
+   function showHidden(id) {
+      alert(`oui c'est responsive ${id}`);
    }
 
    return (
@@ -45,24 +17,16 @@ function Propos() {
             imageAlt="Banner à propos"
             text=""
          />
-         <>
-            {aboutList.map(({ id, title, content }) => (
-               <div key={id} className="MenuWrapper">
-                  <Collapse title={title} showHidden={() => handleClick(id)} />
-                  <div
-                     value={`isShown${id}`}
-                     className="AproposHiddenContent"
-                     style={
-                        `isShown${id}`
-                           ? { display: "none" }
-                           : { display: "flex" }
-                     }
-                  >
-                     {content}
-                  </div>
-               </div>
-            ))}
-         </>
+
+         {aboutList.map(({ id, title, content }) => (
+            <Collapse
+               key={id}
+               title={title}
+               content={content}
+               showHidden={() => showHidden(id)}
+               isShown={false}
+            />
+         ))}
       </div>
    );
 }
