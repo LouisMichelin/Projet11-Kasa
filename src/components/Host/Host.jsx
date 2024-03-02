3f5935b20f0fd903edd3d6ebd8a3c8452878b081
LouisMichelin <louis.michelin097@gmail.com>
2024-03-02 18:19:17 +0100

    Logement-Host : setup initial (sauvegarde)

---
+++ src/components/Host/Host.jsx
@@ -0,0 +1,12 @@
+import "./Host.scss";
+
+function Host() {
+   return (
+      <div className="HostWrapper">
+         <div>JE SUIS LA PHOTO & LE PRENOM</div>
+         <div>JE SUIS LE RATING</div>
+      </div>
+   );
+}
+
+export default Host;
--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -4,13 +4,13 @@ import Carrousel from "../../components/Carrousel/Carrousel";
 import Renseignements from "../../components/Renseignements/Renseignements";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import Host from "../../components/Host/Host";
 
 function Logement() {
    const { index } = useParams();
    const [currentIndex, setCurrentIndex] = useState(0);
    let lastImg = parseInt(ListeLogements[index].pictures.length);
    let selectedLogement = ListeLogements[index];
-
    function handleClickLeft() {
       if (currentIndex > 0) {
          return setCurrentIndex(currentIndex - 1);
@@ -21,7 +21,6 @@ function Logement() {
          return setCurrentIndex(currentIndex + 1);
       }
    }
-
    return (
       <div className="LogementWrapper" key={selectedLogement.id}>
          <Carrousel
@@ -30,7 +29,6 @@ function Logement() {
             totalImg={lastImg}
             toggleArrowLeft={() => handleClickLeft()}
             toggleArrowRight={() => handleClickRight()}
-            // cle={selectedLogement.id}
          />
          <div className="LogementExplications">
             <Renseignements
@@ -39,7 +37,9 @@ function Logement() {
                allTags={selectedLogement.tags}
                cle={selectedLogement.id}
             />
-            <div className="LogementHost">Proprio (A FAIRE EN COMPONENT)</div>
+            <div className="LogementHost">
+               <Host />
+            </div>
          </div>
          <div className="LogementCollapses">
             <div>BOUTON1</div>
