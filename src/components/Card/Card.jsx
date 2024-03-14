e138b910ecd7830e75a42c95d0398fb2949bc47d
LouisMichelin <louis.michelin097@gmail.com>
2024-03-14 17:14:15 +0100

    Card.jsx : Remove useless "onClick" (<Link/> already does the job)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import "./Card.scss";
 import { Link } from "react-router-dom";
 
-function Card({ title, cardImageAlt, cardImageSrc, handleClick, pageIndex }) {
+function Card({ title, cardImageAlt, cardImageSrc, pageIndex }) {
    return (
-      <Link className="CardStructure" onClick={handleClick} to={pageIndex}>
+      <Link className="CardStructure" to={pageIndex}>
          <img className="CardImage" alt={cardImageAlt} src={cardImageSrc} />
          <div className="CardTitle">{title}</div>
       </Link>
